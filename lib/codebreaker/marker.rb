6564e9cd10a62d03605edb7664c7a78f6d8e276f
Jeffrey Gunderson <jeffrey.gunderson@gmail.com>
2014-11-07 12:06:11 -0700

    Duplicate code elements don't lead to multiple '-'

--- lib/codebreaker/marker.rb
+++ lib/codebreaker/marker.rb
@@ -9,9 +9,7 @@ class Marker
   end
 
   def number_match_count
-    (0..3).inject(0) do |count, index|
-      count + (number_match?(index) ? 1 : 0)
-    end
+    total_match_count - exact_match_count
   end
 
   def exact_match?(index)
@@ -20,5 +18,15 @@ class Marker
 
   def number_match?(index)
     @secret.include?(@guess[index]) && !exact_match?(index)
+  end
+
+  def total_match_count
+    secret = @secret.split('')
+    @guess.split('').inject(0) do |count, n|
+      count + (delete_first(secret, n) ? 1 : 0)
+    end
+  end
+  def delete_first(code, n)
+    code.delete_at(code.index(n)) if code.index(n)
   end
 end
